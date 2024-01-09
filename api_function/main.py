4e5224aeac384d7b62b79deda12af84e3954799a
Hannah Gordon <hannahgordon@sky.com>
2024-01-09 11:03:39 +0000

    Get metadata if available

--- api_function/main.py
+++ api_function/main.py
@@ -5,7 +5,17 @@ def get_video_from_id(id):
     db = firestore_v1.Client(project='scareflix', database='scareflix-db')
     path = "videos/{}".format(id)
     doc = db.document(path).get()
-    return {'id':id, 'data': doc.to_dict()}
+    return {'id':id, 'data': doc.to_dict(), 'metadata': get_metadata(doc, db.collection("tmdb_metadata"))}
+
+def get_metadata(doc, collection_ref):
+    metadata_id = doc.to_dict().get('tmdb_metadata_id')
+    if metadata_id:
+        metadata_ref = collection_ref.document(metadata_id)
+        metadata = metadata_ref.get()
+        if metadata.exists:
+            return metadata.to_dict()
+    return None
+
 
 @functions_framework.http
 def get_video(request):
