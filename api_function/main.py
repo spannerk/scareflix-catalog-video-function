cbbaadaf6ea5e7396f56eb2220ee5b9e836fcb47
Hannah Gordon <hannahgordon@sky.com>
2024-01-10 18:26:41 +0000

    Fix: Does the doggie die? Typo and add kws

--- api_function/main.py
+++ api_function/main.py
@@ -8,7 +8,7 @@ def get_video_from_id(id):
     return {'id':id,
             'data': doc.to_dict(),
             'metadata': get_metadata(doc, db.collection("tmdb_metadata")), 
-            'trigger_warnings': get_trigger_warnings(doc, db.collection("ddtd_metadata"))}
+            'trigger_warnings': get_trigger_warnings(doc, db.collection("dtdd_metadata"))}
 
 def get_metadata(doc, collection_ref):
     metadata_id = doc.to_dict().get('tmdb_metadata_id')
@@ -16,11 +16,20 @@ def get_metadata(doc, collection_ref):
         metadata_ref = collection_ref.document(metadata_id)
         metadata = metadata_ref.get()
         if metadata.exists:
-            return metadata.to_dict()
+            return_val = metadata.to_dict()
+            return_val['keywords'] = get_keywords(metadata.to_dict()['data'])
+            return return_val
     return None
 
+def get_keywords(metadata):
+    keywords = metadata.get('keywords')
+    if keywords:
+        return [k['name'] for k in keywords['keywords']]
+    return []
+
+
 def get_trigger_warnings(doc, collection_ref):
-    metadata_id = doc.to_dict().get('ddtd_metadata_id')
+    metadata_id = doc.to_dict().get('dtdd_metadata_id')
     if metadata_id:
         metadata_ref = collection_ref.document(metadata_id)
         metadata = metadata_ref.get()
